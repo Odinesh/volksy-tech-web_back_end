a99709609dac9937c9c12c0563b20dbf5c170958
Odinesh <dinesho@brightcomgroup.com>
2023-03-02 04:27:31 +0000

    7-load_balancer.js

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,16 +1,3 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userData = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'fulfilled',
-    value: data,
-  }));
-
-  const fileData = await uploadPhoto(fileName).catch((err) => ({
-    status: 'rejected',
-    value: err.toString(),
-  }));
-
-  return Promise.resolve([userData, fileData]);
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
 }
