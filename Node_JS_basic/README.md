139e3ec63cce3905bca3ef2922889d5b3c6a6ed1
Odinesh <dinesho@brightcomgroup.com>
2023-03-26 23:54:04 -0700

    hii

--- Node_JS_basic/README.md
+++ Node_JS_basic/README.md
@@ -1,1 +1,1 @@
-Node_Js_basic
+Taskk
--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+controllerRouting(app);
 
 app.listen(port, () => {
   //   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-module.exports = app;
+export default app;
